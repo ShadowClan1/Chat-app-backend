ae518a1ea7793756b9f70be997af558dd77ab382
Ritik Kumar <109942542+ShadowClan1@users.noreply.github.com>
2023-07-06 09:51:16 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose")
 
 const connectDb = async () =>{
-return await mongoose.connect('mongodb://127.0.0.1:27017/group_chat')
+return await mongoose.connect('mongodb+srv://Krishna:Ritik@jsk-tech.ckiehuu.mongodb.net/group_chat')
 }
 
 module.exports = connectDb
